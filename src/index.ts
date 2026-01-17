077c7c7949d8f502f3cd34ce16456ebf4e30ffcb
Laddge <dev.laddge@gmail.com>
2026-01-18 08:53:20 +0900

    create api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import { Hono } from 'hono'
+import * as cheerio from 'cheerio'
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/:user/:itemId', async (c) => {
+  const { user, itemId } = c.req.param()
+  const res = await fetch(`https://qiita.com/${user}/items/${itemId}`)
+  if (!res.ok) return
+  const $ = cheerio.load(await res.text())
+  return await fetch($('meta[property="og:image"]').attr('content') || '')
 })
 
 export default app
